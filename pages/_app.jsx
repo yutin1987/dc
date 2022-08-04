a25809f48ce605176d668affbf54bd6224049529
YuTin <yuting1987@gmail.com>
2022-08-04 12:29:48 +0000

    feat(web): added globals.css

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
+
+export default function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.node.isRequired,
+  pageProps: PropTypes.shape().isRequired,
+};
